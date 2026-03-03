b240e3ab644544116cad7785334fa44a2216cc96
valaseklukas-svg <valasek.lukas@gmail.com>
2026-03-03 15:21:39 +0100

    Update models.py

--- pieces/CreateIgnitionPiece/models.py
+++ pieces/CreateIgnitionPiece/models.py
@@ -2,12 +2,13 @@ from pydantic import BaseModel, Field
 
 class InputModel(BaseModel):
     gps_text: str = Field(
-        default="48.18401806101684, 17.073830458190876", 
-        description="Vlož GPS súradnice z Google Maps (napr. 48.184, 17.073)."
+        default="49.06565617666292, 18.49023988036085", 
+        description="Vloz GPS suradnice z Google Maps (napr. 48.184, 17.073)."
     )
 
 class OutputModel(BaseModel):
     ignition_shp_path: str = Field(
         ..., 
-        description="Cesta k vygenerovanému ignition shapefile v systéme S-JTSK"
+        description="Cesta k vygenerovanemu ignition shapefile v systeme S-JTSK"
     )
+
