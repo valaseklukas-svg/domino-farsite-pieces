9e4f11f97db0e3b7837eebe9ff21358539519808
valaseklukas-svg <valasek.lukas@gmail.com>
2026-03-03 11:25:31 +0100

    Update models.py

--- pieces/CreateIgnitionPiece/models.py
+++ pieces/CreateIgnitionPiece/models.py
@@ -1,12 +1,23 @@
 from pydantic import BaseModel, Field
+from enum import Enum
+
+# Definícia možností pre rolovacie menu
+class EpsgCode(str, Enum):
+    wgs84 = "4326"
+    sjtsk = "5514"
 
 class InputModel(BaseModel):
     """
     Input model pre fiktívne generovanie Ignition Shapefile
     """
     gps_text: str = Field(
-        default="48.1486, 17.1077", 
-        description="Fiktívne GPS súradnice na otestovanie workflowu (Lat, Lon)"
+        default="17.1077, 48.1486", 
+        description="Súradnice oddelené čiarkou vo formáte X, Y (Lon, Lat pre WGS84 alebo Východ, Sever pre S-JTSK)."
+    )
+    
+    epsg_code: EpsgCode = Field(
+        default=EpsgCode.wgs84,
+        description="Súradnicový systém (EPSG kód) pre vygenerovaný shapefile."
     )
 
 class OutputModel(BaseModel):
