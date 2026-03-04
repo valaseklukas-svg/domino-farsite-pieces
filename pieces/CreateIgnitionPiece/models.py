297b7deb8e1ac1f37bda91deab06410836b249b9
valaseklukas-svg <valasek.lukas@gmail.com>
2026-03-04 09:11:39 +0100

    Update models.py

--- pieces/CreateIgnitionPiece/models.py
+++ pieces/CreateIgnitionPiece/models.py
@@ -2,8 +2,12 @@ from pydantic import BaseModel, Field
 
 class InputModel(BaseModel):
     gps_text: str = Field(
-        default="49.06565617666292, 18.49023988036085", 
+        default="49.066, 18.490", 
         description="Vloz GPS suradnice z Google Maps (napr. 48.184, 17.073)."
+    )
+    lcp_path: str = Field(
+        ..., 
+        description="Cesta k .lcp suboru na disku pre kontrolu hranic uzemia (napr. /home/shared_storage/.../final.lcp)."
     )
 
 class OutputModel(BaseModel):
@@ -11,4 +15,5 @@ class OutputModel(BaseModel):
         ..., 
         description="Cesta k vygenerovanemu ignition shapefile v systeme S-JTSK"
     )
+
 
